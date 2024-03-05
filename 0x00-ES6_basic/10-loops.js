b455ee0ccc663ef88457208f5944a6dafb20b46b
Sadson <sadsonmkumira@yahoo.com>
2024-03-05 06:42:25 +0000

    Adding file adjustments to 6-string

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const value of array) {
-    value += appendString;
+  for (const idx of array) {
+    const value = array[idx];
+    array[idx] = appendString + value;
   }
 
   return array;
