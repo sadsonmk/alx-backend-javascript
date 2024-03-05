3e575f5e720848bd7efd145d8fa7f3ec654d0134
Sadson <sadsonmkumira@yahoo.com>
2024-03-05 07:38:14 +0000

    Adding files

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
   for (const idx of array) {
-    const value = array[idx];
-    newArray[idx] = appendString + value;
+    const value = appendString + idx;
+    newArray.push(value);
   }
 
   return newArray;
