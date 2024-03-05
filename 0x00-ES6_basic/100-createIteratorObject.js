e0a419564ef3657b52dd4b6a1a369fed87c177ef
Sadson <sadsonmkumira@yahoo.com>
2024-03-05 08:17:58 +0000

    Adding the 100-createIterator

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,4 +1,6 @@
 export default function createIteratorObject(report) {
   const value = Object.values(report);
-  ;
+  const items = Object.values(value[0]);
+  const newArray = items.flat();
+  return newArray;
 }
