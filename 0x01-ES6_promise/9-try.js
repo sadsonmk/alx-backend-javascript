1a2aabab11cbdb2af101024152012d33c7a24e39
Sadson <sadsonmkumira@yahoo.com>
2024-03-06 09:42:16 +0000

    Adding files

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,11 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const value = mathFunction();
+    queue.push(value);
+  } catch (error) {
+    queue.push(error.message);
+  }
+  queue.push('Guardrail was processed');
+  return queue;
+}
