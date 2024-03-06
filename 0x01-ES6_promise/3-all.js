9a234eb98ccb17eaea6c43b2bed36f452285048f
Sadson <sadsonmkumira@yahoo.com>
2024-03-06 07:59:23 +0000

    Adding return to the file 3-all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then((data) => {
+  return Promise.all([uploadPhoto(), createUser()]).then((data) => {
     console.log(data[0].body, data[1].firstName, data[1].lastName);
   }).catch(() => {
-    console.log('Signup system offline');
+    console.error('Signup system offline');
   });
 }
