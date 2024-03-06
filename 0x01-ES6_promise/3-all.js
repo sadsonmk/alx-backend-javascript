7721882338a4efe217b18e57ee483d262d9387ad
Sadson <sadsonmkumira@yahoo.com>
2024-03-06 08:08:13 +0000

    Adding files

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,6 +4,6 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then(([photo, user]) => {
     console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
   }).catch(() => {
-    console.error('Signup system offline');
+    console.log('Signup system offline');
   });
 }
