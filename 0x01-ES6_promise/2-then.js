2a2194a9863af70d3a58c3ed2254ccf771168acd
Sadson <sadsonmkumira@yahoo.com>
2024-03-06 07:11:27 +0000

    Adding files

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  if (promise) {
+  return promise.then(() => {
     console.log('Got a response from the API');
-    return {
-      status: 200,
-      body: 'Success',
-    };
-  }
-  return new Error();
+    return { status: 200, body: 'success' };
+  }).catch(() => {
+    console.log('Got a response from the API');
+    return new Error();
+  });
 }
