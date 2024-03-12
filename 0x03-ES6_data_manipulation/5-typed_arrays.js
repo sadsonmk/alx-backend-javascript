87b01b12d8a27075138a1839e7fbf3f6d0256429
Sadson <sadsonmkumira@yahoo.com>
2024-03-12 09:54:14 +0000

    Adding an improved version that returns a dataview

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -6,5 +6,5 @@ export default function createInt8TypedArray(length, position, value) {
   const int8view = new Int8Array(buffer);
   int8view[position] = value;
 
-  return buffer;
+  return new DataView(buffer);
 }
