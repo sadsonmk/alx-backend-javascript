e742224dcf6d6f836e81085999e6120b5d481141
Sadsonmk <sadsonmkumira@yahoo.com>
2024-04-30 01:04:30 -0700

    Adding file

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,11 +2,14 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // read user input
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
 
-  console.log(`Your name is: ${name}`);
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
 
+process.stdin.on('close', () => {
   process.stdout.write('This important software is now closing\n');
-  process.exit(0);
 });
