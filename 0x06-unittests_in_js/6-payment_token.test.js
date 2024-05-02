a9dc789d98050aff2c2031461f30bd13d1120701
Sadsonmk <sadsonmkumira@yahoo.com>
2024-05-02 07:38:22 -0700

    Adding the 6-payment_token.test

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,25 @@
+const sinon = require('sinon');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('resolve a promise when success is true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((data) => {
+        expect(data).to.deep.equal({data: 'Successful response from the API' });
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it('should not resolve the promise when success is false', (done) => {
+    getPaymentTokenFromAPI(false)
+    .then(() => {
+      done(new Error('Promise should not be resolved'));
+    })
+    .catch(() => {
+      done();
+    });
+  });
+});
